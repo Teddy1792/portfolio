d0696eeac1ef704836c58a84fa4ace7c1f171adf
Theodore Abitbol <theodore.abitbol@gmail.com>
2023-12-11 20:01:42 +0100

    new start

--- src/techLayer/actions.js
+++
@@ -1,7 +0,0 @@
-// Define action
-export const TOGGLE_DARK_MODE = 'TOGGLE_DARK_MODE';
-
-// define action creator using the constant
-export const toggleDarkMode = () => ({
-  type: TOGGLE_DARK_MODE,
-});
